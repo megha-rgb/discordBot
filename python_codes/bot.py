c0c5960308d9c8fa3d913ce639ec7c9eb8b895b3
Niick <niickchu@gmail.com>
2023-04-27 11:55:43 -0700

    Add simple bot driver code

---
+++ python_codes/bot.py
@@ -0,0 +1,42 @@
+import discord
+import botResponses
+
+class VeggieBot():
+    def __init__(self, dictionary):
+        self.responses = botResponses.BotResponse(dictionary)
+        self.run_bot()
+        
+    def run_bot(self):
+
+        intents = discord.Intents.default()
+        intents.message_content = True
+        client = discord.Client(intents=intents)
+
+        
+
+        @client.event
+        async def on_ready():
+            print(f'{client.user} is now running!')
+
+        @client.event
+        async def on_message(message):
+
+            # # Get data about the user
+            # username = str(message.author)
+            # user_message = str(message.content)
+            # channel = str(message.channel)
+            # print(f"{username} said: '{user_message}' ({channel})")
+
+            if message.author == client.user:
+                return
+            
+            response = self.responses.getResponse(message.content)
+            if response is not None:
+                await message.channel.send(response)
+
+
+        client.run('MTEwMTAwNjA1OTIzOTM3OTAwNA.GYziJQ.LuPKEdYOBGsMycK6IT-RaTh3iEB91FYL8HHEps')  #super secure, definitely no issue with having the key here
+
+
+            
+
